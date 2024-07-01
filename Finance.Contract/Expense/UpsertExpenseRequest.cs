36f3a3178bcf2a4927e3b5d2fe0ed55c97e21f6a
Beka Birhanu <beka.b.atomsa@gmail.com>
2024-07-01 18:51:42 +0300

    feat(contract): add custom  model validation using DataAnnotations

--- Finance.Contract/Expense/UpsertExpenseRequest.cs
+++ Finance.Contract/Expense/UpsertExpenseRequest.cs
@@ -1,7 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Finance.Contract.Expense;
 
 public record UpsertExpenseRequest(
+    [Required(ErrorMessage = "Title is required.")]
     string title,
+
+    [Range(0.01, float.MaxValue, ErrorMessage = "Amount must be greater than 0.")]
     float amount,
+
+    [Required(ErrorMessage = "Date is required.")]
     DateTime date
-    );
+);
+
