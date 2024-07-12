cd26dd98f72c754636e21473d16a5cbf9a648e29
Beka Birhanu <beka.b.atomsa@gmail.com>
2024-07-12 11:23:27 +0300

    feat(contract): add input character inclusion validation on register reqest

--- Finance.Contract/Auth/UserRegistrationRequest.cs
+++ Finance.Contract/Auth/UserRegistrationRequest.cs
@@ -4,14 +4,17 @@ namespace Finance.Contract.Auth;
 
 public record UserRegistrationRequest(
   [Required(ErrorMessage = "firstName is required")]
+  [RegularExpression(@"^[a-zA-Z0-9_]$", ErrorMessage = "Invalid firstName format")]
   string firstName,
 
   [Required(ErrorMessage = "lastName is required")]
+  [RegularExpression(@"^[a-zA-Z0-9_]$", ErrorMessage = "Invalid lastName format")]
   string lastName,
 
   [RegularExpression(@"^[a-zA-Z0-9_.]+@[a-zA-Z0-9-]+\.[a-zA-Z]{2,}$", ErrorMessage = "Invalid email format")]
   string email,
 
   [MinLength(8)]
+  [RegularExpression(@"^[A-Za-z\d@$!%*?&]+$", ErrorMessage = "Invalid password format")]
   string password
 );
